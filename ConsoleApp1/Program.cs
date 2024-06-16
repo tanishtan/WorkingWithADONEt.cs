1821312dbe2b8dcd57879c12d65cc44f9c349803
Tanish K <tanishkunigiri2002@gmail.com>
2024-06-16 16:33:27 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.SqlClient;
+
+public class TestClass
+{
+    static void Main()
+    {
+        var connStr = "Server=tcp:tanishdbserver.database.windows.net,1433;Initial Catalog=TanishDB;Persist Security Info=False;User ID=tanishdbadmin;Password=Tanish@2002;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+        SqlConnection con = new SqlConnection();
+
+
+        con.ConnectionString = connStr;
+        con.Open();
+
+        var cmd = con.CreateCommand();
+        cmd.CommandText = "Select * from Employee";
+
+        var reader = cmd.ExecuteReader(); // Use SqlCommand.ExecuteReader
+
+        while (reader.Read())
+        {
+            Console.WriteLine("{0}, {1}, {2}", reader[0], reader[1], reader[2]);
+        }
+        con.Close();
+    }
+}
